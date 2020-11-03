e995601e69acabf36e4fda9638013d5b9348fd2d
k.sawada <63055099+koukisawada1@users.noreply.github.com>
2020-11-03 16:50:25 +0900

    Add:管理者側入庫登録ページ作成

--- resources/views/admin/receipt/show.blade.php
+++ resources/views/admin/receipt/show.blade.php
@@ -2,19 +2,45 @@
 @section('content')
 
 <div class="receipt-show">
-    <div class="recipt-new">
+    <div class="receipt-new">
         <form method="POST" action="{{route('admin.receipt.store')}}">
             @csrf
+            <div class="header">
+               <h2>入庫入力</h2> 
+            </div>
             <p>{{ $item->item_name }}</p>
-            <input type="number" name="num">
+            <p><span>在庫：</span>{{ $item->num }}</p>
+            <p>入庫数：<input type="number" name="num"></p>
             <input type="hidden" name="item_id" value="{{ $item->id }}">
             <div class="receipt-btn">
                 <input type="submit" value="送信する">
-            </div>
-            
+            </div>   
         </form>
     </div>
     <div class="receipt-index">
+        <div class="header">
+            <h2>[{{ $item->item_name }}]入庫履歴</h2>
+        </div>
+        <table>
+            <tr>
+                <th>
+                    <p>登録日</p>
+                </th>
+                <th>
+                    <p>個数</p>
+                </th>
+            </tr>
+            @foreach ($item->receipts as $receipt)
+            <tr>
+                <td>
+                    <p>{{ $receipt->created_at }}</p>
+                </td>
+                <td>
+                    <p>{{ $receipt->num }}</p>
+                </td>
+            </tr>
+            @endforeach
+        </table>
         
     </div>
 </div>
