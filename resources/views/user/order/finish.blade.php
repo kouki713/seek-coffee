7e8de2a0e8e3894d9031543c270fba194e586e38
k.sawada <63055099+koukisawada1@users.noreply.github.com>
2020-11-05 18:21:16 +0900

    Add:注文完了ページ作成

--- resources/views/user/order/finish.blade.php
+++ resources/views/user/order/finish.blade.php
@@ -1,4 +1,19 @@
-完了画面
+
 @extends('layouts.user.app')
 @section('content')
+<div class="content-header">
+    <div class="header-nav">
+        <p>カート</p>
+        <span><i class="fas fa-chevron-right"></i></span>
+        <p>購入情報入力</p>
+        <span><i class="fas fa-chevron-right"></i></span>
+        <p>確認画面</p>
+        <span><i class="fas fa-chevron-right"></i></span>
+        <h4 class="order-status">完了</h4>
+    </div>
+</div>
+<div class="finish">
+    <p>ご購入ありがとうございます。</p>
+    <a href="{{ route('home.index') }}">トップページへ</a>
+</div>
 @endsection
